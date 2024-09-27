7f85ab4db402071cd0c52b0062381900c32beca6
Adnan Niaz <sanixstudio@gmail.com>
2024-09-27 01:02:07 -0700

    hiding signin buton for now

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,17 +6,17 @@ interface HeaderProps {
   onSearchToggle: () => void;
 }
 
-const Header: React.FC<HeaderProps> = ({ onSignIn, onSearchToggle }) => {
+const Header: React.FC<HeaderProps> = ({ onSearchToggle }) => {
   return (
     <div className="header flex justify-between items-center mb-4">
       <h1 className="text-2xl text-textColor font-bold">Flash Notes</h1>
       <div className="header-actions flex items-center gap-4">
-        <button
+        {/* <button
           className="sign-in-btn bg-buttonBg hover:bg-buttonHover text-textColor p-2 py-1 rounded transition-transform transform hover:scale-105"
           onClick={onSignIn}
         >
           Sign In
-        </button>
+        </button> */}
         <button
           className="search-toggle bg-transparent p-2"
           onClick={onSearchToggle}
