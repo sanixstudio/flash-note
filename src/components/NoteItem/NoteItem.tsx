6b596469dd8e1850b9411ba75169eec9695b4246
Adnan Niaz <sanixstudio@gmail.com>
2024-10-30 18:46:48 -0700

    feat: adding ui/ux changes for improvements

--- src/components/NoteItem/NoteItem.tsx
+++ src/components/NoteItem/NoteItem.tsx
@@ -51,12 +51,6 @@ const NoteItem: React.FC<NoteItemProps> = ({
     }
   }, [isEditing]);
 
-  // const handleNoteClick = (event: React.MouseEvent) => {
-  //   if (!(event.target as HTMLElement).closest(".note-btn")) {
-  //     onToggleCompletion(note.id);
-  //   }
-  // };
-
   const handleCopy = () => {
     onCopy(note.content);
     toast({
@@ -113,73 +107,15 @@ const NoteItem: React.FC<NoteItemProps> = ({
           ref={provided.innerRef}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
-          className={`note bg-bgColor border rounded w-full mb-2 relative
-            ${
-              note.priority
-                ? "border-yellow-400 bg-yellow-400/20"
-                : "border-borderColor"
-            }
-            ${note.pinned ? "border-gray-300/50 bg-gray-300/20" : ""}
-            ${note.completed ? "opacity-50" : ""}
-            transition-transform hover: group`}
+          className={`bg-bgColor border rounded w-full mb-2 relative transition-transform hover:group ${
+            note.priority
+              ? "border-yellow-400 bg-yellow-400/20"
+              : note.completed
+              ? "border-emerald-500/50 bg-emerald-500/10 opacity-30"
+              : "border-borderColor"
+          } ${note.pinned ? "border-gray-300/50 bg-gray-300/20" : ""}`}
         >
-          <style>
-            {`
-              .ql-editor {
-                color: var(--text-color);
-                padding: 0.5rem;
-                background-color: rgba(0, 0, 0, 0.2);
-                border-top-left-radius: 0.25rem;
-                border-top-right-radius: 0.25rem;
-                min-height: fit-content;
-              }
-              .ql-editor p {
-                margin-bottom: 0;
-              }
-              .note-content .ql-editor {
-                background-color: rgba(0, 0, 0, 0.2);
-              }
-              .note-content.completed .ql-editor {
-                text-decoration: line-through;
-              }
-              .note-content .ql-container {
-                border: none;
-              }
-              .editing .ql-editor {
-                background-color: rgba(0, 0, 0, 0.3);
-              }
-              .ql-toolbar {
-                background-color: rgba(0, 0, 0, 0.3);
-                border: none;
-                border-top-left-radius: 0.25rem;
-                border-top-right-radius: 0.25rem;
-              }
-              .ql-toolbar .ql-stroke {
-                stroke: var(--text-color);
-              }
-              .ql-toolbar .ql-fill {
-                fill: var(--text-color);
-              }
-              .ql-toolbar .ql-picker {
-                color: var(--text-color);
-              }
-              .note-btn {
-                opacity: 0.5;
-                transition: opacity 0.2s ease-in-out;
-              }
-              .group:hover .note-btn,
-              .note-btn:focus,
-              .note-btn.active,
-              .editing .note-btn {
-                opacity: 1;
-              }
-              .note-btn.priority-active {
-                color: #fbbf24; /* Tailwind's yellow-400 */
-              }
-            `}
-          </style>
-
-          <div className="flex justify-between items-center bg-gray-800 p-1 rounded-t">
+          <div className="flex justify-between items-center bg-gray-800 p-1 rounded-t-sm">
             <div className="text-xs text-gray-400 ml-1">
               <FaClock size={12} className="inline mr-1" />
               <span>
@@ -190,8 +126,8 @@ const NoteItem: React.FC<NoteItemProps> = ({
             </div>
             <div className="flex items-center space-x-1">
               <button
-                className={`note-btn bg-transparent p-1 rounded ${
-                  note.priority ? "priority-active" : "text-gray-400"
+                className={`bg-transparent p-1 rounded ${
+                  note.priority ? "text-yellow-400" : "text-gray-400"
                 } hover:text-yellow-400`}
                 onClick={(e) => {
                   e.stopPropagation();
@@ -201,8 +137,8 @@ const NoteItem: React.FC<NoteItemProps> = ({
                 <FaStar size={14} />
               </button>
               <button
-                className={`note-btn bg-transparent p-1 rounded ${
-                  note.pinned ? "text-gray-200 active" : "text-gray-400"
+                className={`bg-transparent p-1 rounded ${
+                  note.pinned ? "text-gray-200" : "text-gray-400"
                 } hover:text-gray-100`}
                 onClick={(e) => {
                   e.stopPropagation();
@@ -213,11 +149,7 @@ const NoteItem: React.FC<NoteItemProps> = ({
               </button>
             </div>
           </div>
-          <div
-            className={`note-content ${isEditing ? "editing" : ""} ${
-              note.completed ? "completed" : ""
-            }`}
-          >
+          <div className={`p-2 bg-black/20 ${isEditing ? "bg-black/30" : ""}`}>
             {isEditing ? (
               <ReactQuill
                 ref={quillRef}
@@ -229,30 +161,38 @@ const NoteItem: React.FC<NoteItemProps> = ({
               />
             ) : (
               <div
-                className="ql-editor"
+                className={`ql-editor text-[var(--text-color)] p-2 bg-black/20 rounded-t-sm min-h-fit ${
+                  note.completed ? "line-through" : ""
+                }`}
                 dangerouslySetInnerHTML={{ __html: note.content }}
               />
             )}
           </div>
-          <div className={`note-controls ${isEditing ? "editing" : ""}`}>
+          <div
+            className={`note-controls ${
+              isEditing
+                ? "flex justify-end space-x-2"
+                : "flex justify-end space-x-2"
+            }`}
+          >
             {isEditing ? (
               <div className="flex justify-end space-x-2">
                 <button
-                  className="note-btn bg-transparent p-1 rounded text-gray-300 hover:text-gray-100"
+                  className="bg-transparent p-1 rounded text-gray-300 hover:text-gray-100"
                   onClick={handleSave}
                 >
                   <FaSave size={14} />
                 </button>
                 <button
-                  className="note-btn bg-transparent p-1 rounded text-gray-300 hover:text-gray-100"
+                  className="bg-transparent p-1 rounded text-gray-300 hover:text-gray-100"
                   onClick={handleCancel}
                 >
                   <FaTimes size={14} />
                 </button>
               </div>
             ) : (
-              <div className="flex justify-end space-x-2">
-                <div className="flex items-center pl-2 rounded w-full">
+              <div className="flex justify-end space-x-2 w-full">
+                <div className="w-full p-2">
                   <button
                     className="flex items-center gap-2 text-gray-400 hover:text-gray-100"
                     onClick={(e) => {
@@ -274,7 +214,7 @@ const NoteItem: React.FC<NoteItemProps> = ({
                   </button>
                 </div>
                 <button
-                  className="note-btn bg-transparent p-1 rounded text-gray-400 hover:text-gray-100"
+                  className="bg-transparent p-1 rounded text-gray-400 hover:text-gray-100"
                   onClick={(e) => {
                     e.stopPropagation();
                     handleEdit();
@@ -283,7 +223,7 @@ const NoteItem: React.FC<NoteItemProps> = ({
                   <FaEdit size={14} />
                 </button>
                 <button
-                  className="note-btn bg-transparent p-1 rounded text-gray-400 hover:text-gray-100"
+                  className="bg-transparent p-1 rounded text-gray-400 hover:text-gray-100"
                   onClick={(e) => {
                     e.stopPropagation();
                     handleCopy();
@@ -292,7 +232,7 @@ const NoteItem: React.FC<NoteItemProps> = ({
                   <FaCopy size={14} />
                 </button>
                 <button
-                  className="note-btn bg-transparent p-1 rounded text-gray-400 hover:text-gray-100"
+                  className="bg-transparent p-1 rounded text-gray-400 hover:text-gray-100"
                   onClick={(e) => {
                     e.stopPropagation();
                     onDelete(note.id);
